1039d8020ebe107b108a355eb6232e148a5049a0
unknown <herisetyawan233@gmail.com>
2021-09-17 02:05:10 +0700

    templating belum fix

---
+++ resources/views/partials/sidebar.blade.php
@@ -0,0 +1,79 @@
+<!-- Sidebar -->
+<div class="sidebar sidebar-style-2">			
+    <div class="sidebar-wrapper scrollbar scrollbar-inner">
+        <div class="sidebar-content">
+            <div class="user">
+                <div class="avatar-sm float-left mr-2">
+                    <img src="assets/image/profile.jpg" alt="..." class="avatar-img rounded-circle">
+                </div>
+                <div class="info">
+                    <a data-toggle="collapse" href="#collapseExample" aria-expanded="true">
+                        <span>
+                            Hizrian
+                            <span class="user-level">Administrator</span>
+                            <span class="caret"></span>
+                        </span>
+                    </a>
+                    <div class="clearfix"></div>
+
+                    <div class="collapse in" id="collapseExample">
+                        <ul class="nav">
+                            <li>
+                                <a href="#profile">
+                                    <span class="link-collapse">My Profile</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#edit">
+                                    <span class="link-collapse">Edit Profile</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#settings">
+                                    <span class="link-collapse">Log out</span>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <ul class="nav nav-primary">
+                <li class="nav-item active">
+                    <a href="/">
+                        <i class="fas fa-home"></i>
+                        <p>Dashboard</p>
+                        
+                    </a>
+           
+                </li>
+                <li class="nav-section">
+                    <span class="sidebar-mini-icon">
+                        <i class="fa fa-ellipsis-h"></i>
+                    </span>
+                    <h4 class="text-section">Menu</h4>
+                </li>
+                <li class="nav-item">
+                    <a href="/artikel">
+                        <i class="fas fa-newspaper"></i>
+                        <p>Menu Artikel</p>
+                    </a>
+                </li>
+
+                <li class="nav-item">
+                    <a href="/challenge">
+                        <i class="fas fa-tasks"></i>
+                        <p>Menu Challenge</p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="/membership">
+                        <i class="fas fa-users"></i>
+                        <p>Membership</p>
+                    </a>
+                </li>
+                
+            </ul>
+        </div>
+    </div>
+</div>
+<!-- End Sidebar -->
--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,18 @@ Route::get('/', function () {
     return view('dashboard.index');
 });
 
+Route::get('/artikel', function () {
+    return view('dashboard.artikel.index');
+});
+
+Route::get('/challenge', function () {
+    return view('dashboard.challenge.index');
+});
+
+Route::get('/membership', function () {
+    return view('dashboard.membership.index');
+});
+
 
 Route::get('/login', function(){
     return view('auth.login');
